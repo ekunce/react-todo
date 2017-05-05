34d02d47967d6c9a8bfa587fca1fa5664feee86d
Eduard Kunce <eduard.kunce@gmail.com>
2017-05-05 13:49:06 +0200

    todos reducer and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -25,4 +25,39 @@ describe('Reducers', () => {
         expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id: 1,
+          text: 'Some text',
+          completed: false,
+          createdAt: 1,
+          completedAt: undefined
+        }
+      ];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].completed).toBe(true);
+      expect(res[0].completedAt).toExist();
+    });
+  });
 });
