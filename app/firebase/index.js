dcd435fdb3a47bdd945838c54b550f640481f866
Eduard Kunce <eduard.kunce@gmail.com>
2017-05-09 16:37:15 +0200

    Move firebase credentials into env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-      apiKey: "AIzaSyACYPxp2_L2-nKMFObjcPQHQknTI_YpJnY",
-      authDomain: "kunce-todo-app.firebaseapp.com",
-      databaseURL: "https://kunce-todo-app.firebaseio.com",
-      projectId: "kunce-todo-app",
-      storageBucket: "kunce-todo-app.appspot.com",
-      messagingSenderId: "275272109257"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
 
 
